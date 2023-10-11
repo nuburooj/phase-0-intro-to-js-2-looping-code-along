43fc6fe4c5bc543831c4b32aa52d84ff078ce352
nuburooj <nuburoojkhattak@gmail.com>
2023-10-11 01:25:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+function writeCards(names, event) {
+    let newCards = [];
+    for (let i = 0; i < names.length; i++) {
+        let msg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newCards.push(msg);
+        
+    }
+    return newCards;
+}
+
+function countDown(num){
+while (num >= 0) {
+  console.log(num) 
+  num-- 
+}
+}
